48d8d5c0c740b72e7403ca7363b78a6231719c84
thibault.leroy <thibault.le-roy@saegus.com>
2020-03-03 12:18:29 +0100

    add env variable for api endpoint

--- app/src/resources/characters.ts
+++ app/src/resources/characters.ts
@@ -1,6 +1,6 @@
 import * as axios from 'axios';
 
-const getCharacters = (offset:number, limit: number) => axios.default.get(`http://localhost:3000/v1/characters?offset=${offset}&limit=${limit}`);
+const getCharacters = (offset:number, limit: number) => axios.default.get(`${process.env.API_ENDPOINT}/v1/characters?offset=${offset}&limit=${limit}`);
 
 
 export {
